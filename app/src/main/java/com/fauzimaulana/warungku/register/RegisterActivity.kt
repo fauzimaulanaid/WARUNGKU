4a2b5e62498f98f5ecb77da3fa4de6397d3b70f4
fauzimaulanaid <muhammadfauzi1933@gmail.com>
2022-07-06 00:05:26 +0700

    Register User Success

--- app/src/main/java/com/fauzimaulana/warungku/register/RegisterActivity.kt
+++ app/src/main/java/com/fauzimaulana/warungku/register/RegisterActivity.kt
@@ -1,17 +1,30 @@
 package com.fauzimaulana.warungku.register
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.util.Patterns
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.fauzimaulana.warungku.R
 import com.fauzimaulana.warungku.databinding.ActivityRegisterBinding
+import com.fauzimaulana.warungku.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
     private var _binding: ActivityRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +32,65 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupView()
+        auth = Firebase.auth
+        binding.registerButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            val rePassword = binding.retypePasswordEditText.text.toString()
+            when {
+                email.isEmpty() -> {
+                    binding.emailEditTextLayout.error = resources.getString(R.string.email_empty)
+                }
+                password.isEmpty() -> {
+                    binding.passwordEditTextLayout.error = resources.getString(R.string.password_empty)
+                }
+                rePassword.isEmpty() -> {
+                    binding.retypePasswordEditTextLayout.error = resources.getString(R.string.retype_password_empty)
+                }
+                password.length < 6 -> {
+                    binding.passwordEditTextLayout.error = resources.getString(R.string.password_error)
+                }
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                    binding.emailEditTextLayout.error = resources.getString(R.string.email_invalid_format)
+                }
+                password != rePassword -> {
+                    binding.retypePasswordEditTextLayout.error = resources.getString(R.string.password_different)
+                }
+                else -> {
+                    registerUser(email, password)
+                }
+            }
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //Register success
+                    Log.d(TAG, "registerUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    //Register failed
+                    Log.w(TAG, "registerUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            binding.contentRegister.visibility = View.GONE
+            binding.viewUserCreated.root.visibility = View.VISIBLE
+            val screenTime = 3000L
+            Handler(mainLooper).postDelayed({
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, screenTime)
+        }
     }
 
     private fun setupView() {
@@ -37,5 +109,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        const val TAG = "RegisterActivity"
     }
 }
